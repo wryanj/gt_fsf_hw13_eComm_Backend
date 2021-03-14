c301f5e8bd87603211d8dceba76a400999910316
wryanj <ryanjohnson9685@gmail.com>
2021-03-14 15:19:07 -0400

    updated indexjs in models complete first take at defining models and associations

--- models/index.js
+++ models/index.js
@@ -1,20 +1,39 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+//----------------------------------------------------------------------------------------------
+// IMPORT DEPENDENCIES
+//----------------------------------------------------------------------------------------------
 
-// Products belongsTo Category
+  // Import all models created in the models directory
+  const Product = require('./Product');
+  const Category = require('./Category');
+  const Tag = require('./Tag');
+  const ProductTag = require('./ProductTag');
 
-// Categories have many Products
+//----------------------------------------------------------------------------------------------
+// DEFINE MODEL ASSOCIATIONS
+//----------------------------------------------------------------------------------------------
 
-// Products belongToMany Tags (through ProductTag)
+  // A product belongs to a category (1-1). Adds a foreign key to the source (Product)
+  Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+  });
 
-// Tags belongToMany Products (through ProductTag)
+  // A category has many products in it (M-1). Adds a foreign key to the target (Product))
+  Category.hasMany(Product, {
+    foreignKey: 'category_id'
+  })
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+  // A product may have many tags (M-M) and a tag may have many products
+  Product.belongsToMany(Tag, {through: ProductTag});
+  Tag.belongsToMany(Product, {through:ProductTag});
+
+
+//----------------------------------------------------------------------------------------------
+// EXPORT MODELS FOR USE IN OTHER JS FILES
+//----------------------------------------------------------------------------------------------
+
+  module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+  };
