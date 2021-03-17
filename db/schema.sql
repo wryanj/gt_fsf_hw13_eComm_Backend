3af44bcb8741c13a3537bc9730c8b29db7112d02
wryanj <ryanjohnson9685@gmail.com>
2021-03-17 19:29:05 -0400

    comment updates

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@
 -- SCHEMA
 -------------------------------------------------------------------------------------- 
 
--- Below we just create the database in SQL. Models and seeds to be managed in other directories within this repo
+-- Here we I just create the datbase- models and data seeds are managed in separate directories. 
 
     -- DROP DATABASE
     DROP DATABASE IF EXISTS ecommerce_db;
